0fe137e240fc92a34f17fc1f5896923b4e71ae7f
MPoleto <poleto.mari@gmail.com>
2022-11-03 11:52:16 -0300

    done

--- Program.cs
+++ Program.cs
@@ -1,3 +1,20 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Iphone");
+Smartphone iphone12 = new Iphone("988349743", "Iphone 12", "5641987129687", 64);
+iphone12.Ligar();
+iphone12.ReceberLigacao();
+Console.WriteLine(iphone12.Numero);
+iphone12.InstalarAplicativo("Notion.so");
+
+Console.WriteLine("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+
+Console.WriteLine("Nokia");
+Smartphone nokiaC30 = new Nokia("994857731", "Nokia C30  ", "9871987198736", 64);
+nokiaC30.Ligar();
+nokiaC30.ReceberLigacao();
+nokiaC30.Numero = "992047593";
+Console.WriteLine("Trocando o número para " + nokiaC30.Numero);
+nokiaC30.InstalarAplicativo("Ifood");
+
+
